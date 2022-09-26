3fe464cec6d2b21cfc0c3bec06b50233338d7706
Aubis Sanchez <aubisst@gmail.com>
2022-09-26 16:13:58 -0400

    redux functions for cart are now working

--- src/pages/Private/Checkout.tsx
+++ src/pages/Private/Checkout.tsx
@@ -1,22 +1,36 @@
-import { Box, Grid, Pagination, Stack, Typography } from "@mui/material";
-import { useSelector } from "react-redux";
+import { Box, Button, Pagination, Stack, Typography } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
 import { mockData } from "../../data/mookCartData";
 import { usePagination } from "../../hooks/usePagination";
 import { AppStore } from "../../redux/store";
 import { useState } from 'react';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import { decreaseCartItemAmount, increaseCartItemAmount, removeCartItem } from "../../redux/states/product";
+import RemoveIcon from '@mui/icons-material/Remove';
+import AddIcon from '@mui/icons-material/Add';
+import { CartItemProps } from "../../models";
 
 export const Checkout = () => {
-
+  const dispatch = useDispatch();
   const cart = useSelector( (store: AppStore) => store.product.cart);
   const [page, setPage] = useState<number>(1);
   const itemsPerPage = 3;
-  const { maxPage, jumpToPage, currentData, currentPage } = usePagination( mockData, itemsPerPage );
-  const count = Math.ceil( mockData.length/itemsPerPage )
+  const { jumpToPage, currentData, } = usePagination(cart , itemsPerPage );
+
+  const count = Math.ceil( cart.length/itemsPerPage )
 
   const handlePageJump = ( e:React.ChangeEvent<any> , page:number ) => {
     setPage( page );
     jumpToPage( page );
   };
+
+  const handleRemoveItem = (id:number) => {
+    dispatch( removeCartItem(id) );
+  }
+ 
+  const handleIncreaseItemQuantity = ( id:number) => dispatch( increaseCartItemAmount(id) )
+
+  const handleDecreaseItemQuantity = ( id:number) => dispatch( decreaseCartItemAmount(id) )
 
   return (
     <Box sx={{ width: '100vw', height: '100vh', display:'flex', alignItems:"center"}}>
@@ -26,31 +40,46 @@ export const Checkout = () => {
         <Box 
           sx={{ width: '75%', maxHeight:'100%', position: 'relative', display:"flex", flexDirection:'column', border:2, borderColor:'gray', background: '#fff', boxShadow:4}}
         >
-        <Box sx={{ width:'100%', height:"30px", display:"grid", gridTemplateColumns: 'repeat(5, minmax(0, 1fr))', color:'rgb(55 65 81)', py:1, borderBottom:2, borderColor:'rgba(128,128,128,0.25)'}}>
-          <Typography variant='body1' align='left' sx={{ gridColumn: 'span 2 / span 2', pl:2 }}> Product Details</Typography>
-          <Typography variant='body1' align='center' sx={{ pl:2 }} > Quantity</Typography>
-          <Typography variant='body1' align='center' sx={{ pl:2 }}>Price by unit</Typography>
-          <Typography variant='body1' align='center' sx={{ pl:2 }}> Total Price </Typography>
+        <Box sx={{ width:'100%', height:"30px", display:"grid", gridTemplateColumns: 'repeat(7, minmax(0, 1fr))', color:'rgb(55 65 81)', py:1, borderBottom:2, borderColor:'rgba(128,128,128,0.25)'}}>
+          <Typography variant='body1' align='left' sx={{ gridColumn: 'span 3 / span 3', pl:1 }}> Product Details</Typography>
+          <Typography variant='body1' align='center' > Quantity</Typography>
+          <Typography variant='body1' align='center' >Price by unit</Typography>
+          <Typography variant='body1' align='center' > Total Price </Typography>
+          <Typography variant='body1' align='center' > Options </Typography>
         </Box>
         <Stack sx={{ height:"calc(100% - 62px)" }} spacing={1}>
           {
-            // cart.map( cartItem => {
-            //   console.log(cartItem);
-            //   return (
-            //     <p>a</p>
-            //   )
-            // })
-            currentData().map( (cartItem: any) => 
-              <Box key={cartItem.id} sx={{  height: '29%', width:'100%', display:"grid", gridTemplateColumns: 'repeat(5, minmax(0, 1fr))', color:'black', borderBottom:1, borderColor:'rgba(128,128,128,0.25)'}}>
-                <Box sx={{ gridColumn: 'span 2 / span 2', display:"flex", flexDirection:"row", alignItems:"center"}}>
-                  <img src={cartItem.category.image} alt={cartItem.title} style={{ height:'145px', minWidth:'60%', paddingLeft:"8px", objectFit:"cover"}}/>
-                  <Typography variant="subtitle1" align='center'  sx={{width:"40%"}}> {cartItem.title} </Typography>
+            currentData().map( ({item, amount, size}: CartItemProps) => (
+              <Box key={item.id} sx={{ height: '29%', width:'100%', display:"grid", gridTemplateColumns: 'repeat(7, minmax(0, 1fr))', color:'black', borderBottom:1, borderColor:'rgba(128,128,128,0.25)'}}>
+                <Box sx={{ gridColumn: 'span 3 / span 3', display:"flex", flexDirection:"row", alignItems:"center", justifyContent:"space-between"}}>
+                  <img src={item.image} alt={item.title} style={{ height:'145px',width:"auto", paddingLeft:"8px", objectFit:"cover", margin:"auto"}}/>
+                  <Box sx={{ display:"flex", justifyContent:"center", flexDirection:"column", alignItems:"center", width:"40%"}}>
+                    <Typography variant="subtitle1" align='center'> {item.title} </Typography>
+                    <Typography variant="subtitle2" align='center' sx={{color:"darkslategray"}}> Size: {size} </Typography>
+                  </Box>
+                </Box >
+                <Box sx={{ display:"flex", alignItems:"center", justifyContent:"center" }}>
+                  <Button
+                    onClick={ () => handleIncreaseItemQuantity(item.id) }
+                  >
+                    <AddIcon/>
+                  </Button>
+                  <Typography variant="subtitle1" > {amount} </Typography>
+                  <Button
+                    onClick={ () => handleDecreaseItemQuantity(item.id) }
+                  > 
+                    <RemoveIcon/>
+                  </Button>
                 </Box>
-                <Typography variant="subtitle1" sx={{ display:"flex", alignItems:"center", justifyContent:"center" }}> {cartItem.id} </Typography>
-                <Typography variant="subtitle1"  sx={{ display:"flex", alignItems:"center", justifyContent:"center" }}> ${cartItem.price} </Typography>
-                <Typography variant="subtitle1" sx={{ display:"flex", alignItems:"center", justifyContent:"center" }}> ${cartItem.price * cartItem.id} </Typography>
+                <Typography variant="subtitle1"  sx={{ display:"flex", alignItems:"center", justifyContent:"center" }}> ${item.price} </Typography>
+                <Typography variant="subtitle1" sx={{ display:"flex", alignItems:"center", justifyContent:"center" }}> ${item.price * amount} </Typography>
+                <Button
+                  onClick={ () => handleRemoveItem(item.id) } 
+                  sx={{ display:"flex", alignItems:"center", justifyContent:"center", margin:"auto"}}>
+                  <HighlightOffIcon />
+                </Button>
               </Box>
-            )
+            ))
           }
         </Stack>
         <Stack spacing={2} sx={{height:"32px", width:"100%", position:"absolute", bottom:10, display:"flex", justifyContent:"center", alignItems:"center",}}>
